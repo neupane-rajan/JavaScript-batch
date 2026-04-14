c2d8107860d2059904162d30ea27b316ac625b46
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-14 13:44:54 +0545

    arrary started

---
+++ array/01_array.js
@@ -0,0 +1,7 @@
+// array
+// creating empty array
+// const myArray = []
+// javascript array are resizable and can contain mix of different datatypes
+//javascript array are zero-indexed(index start with zero)
+
+const myArray = [0, 1, 2, 3, 4, 5];
