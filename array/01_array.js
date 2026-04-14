e9bac55a7fa14ba52efa1c7b7a7fe06c760b6b5e
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-14 13:56:05 +0545

    learning about array methods

--- array/01_array.js
+++ array/01_array.js
@@ -3,5 +3,21 @@
 // const myArray = []
 // javascript array are resizable and can contain mix of different datatypes
 //javascript array are zero-indexed(index start with zero)
-
+// JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
 const myArray = [0, 1, 2, 3, 4, 5];
+
+// accessing array elements
+first_element = myArray[0];
+// console.log(first_element);
+
+const language = ["Nepali", "English", "Hindi", "japnese", "Mandarin"];
+
+// const myArr = new Array(1, 2, 3, 4, 5);
+
+
+
+// Array methods
+
+myArray.push(6)
+
+console.log(myArray);
