01d2c047f78173cb76395ea333b0c5e437b0f75b
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-20 17:52:36 +0545

    idk what to write

--- object/02_object.js
+++ object/02_object.js
@@ -1,4 +1,7 @@
 // let talk about singleton and constructor
-
 const niijoUser = new Object();
+niijoUser.id = "123abcd";
+niijoUser.name = "Random";
+niijoUser.isLoggedIn = false;
+
 console.log(niijoUser);
