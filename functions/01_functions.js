7cda5b721e2e0446550d8a46e84d23f5ad90ad99
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-21 14:28:27 +0545

    function started

---
+++ functions/01_functions.js
@@ -0,0 +1,9 @@
+// syntax of function declaration
+function functionName(parameters) {
+  // code to be executed
+}
+function greeting() {
+  console.log("hello this is javascript course");
+}
+
+greeting();
