a5a3d9ba97a8e1a844f108223490476392fd8d06
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-21 14:23:23 +0545

    03_object.js completed

--- object/03_object.js
+++ object/03_object.js
@@ -9,4 +9,16 @@ const course = {
 // here in above code we have used object destructuring to extract the value of tutor property from course object and assign it to a variable name tutor.
 // if we want to assign different variable name than the property name then we can use colon(:) to assign different variable name like this
 const { tutor: teacher } = course;
-console.log(teacher);
+// console.log(teacher);
+
+// JSON
+
+// {
+//     name:"Rajan",
+//     age: "18",
+//     add:"antartica",
+
+// }
+
+// sometime we can get api on the array formate liek this
+// [{}, {}, {}, {}];
